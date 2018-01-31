95e207055c57dc4e6bb93d86ec70eaadfb743939
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-01-31 19:38:13 +0300

    upd calc

--- 2 SimpleOperations.py
+++ 2 SimpleOperations.py
@@ -5,19 +5,11 @@ print("Вас зовут "+name+"") #function print
 print("Вам "+age+" лет")
 
 #calc
-n1 = input("Введите первое число: ")
-n2 = input("Введите второе число: ")
-a = int(n1)
-b = int(n2)
-r1 = (a + b) #sum n1 and n2
-r2 = (a - b)
-r3 = (a * b)
-r4 = (a ** b)
-r5 = (a / b)
-r6 = (a % b)
-print("Сумма чисел: " + str(r1))#print sum and convert sum from int to string
-print("Разность чисел: " + str(r2))
-print("Произведение чисел: " + str(r3))
-print("a в степени b: " + str(r4))
-print("Деление чисел: " + str(r5))
-print("Остаток от деления первого числа на второе: " + str(r6))
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+print("Сумма чисел:", a + b)
+print("Разность чисел:", a - b)
+print("Произведение чисел:", a * b)
+print("a в степени b:", a ** b)
+print("Деление чисел:", a / b)
+print("Остаток от деления первого числа на второе:", a % b)
