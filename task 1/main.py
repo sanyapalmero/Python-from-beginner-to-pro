4ca025879b7199827bd1870d48c0aed7fabba8b8
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-02-02 01:00:42 +0300

    upd code x2, delete i=0 and others

--- task 1/main.py
+++ task 1/main.py
@@ -1,22 +1,18 @@
 def create_array():
     input_array = []
     array_len = int(input("Введите длину массива: "))
-    i = 0
     for i in range(array_len):
         elem = int(input(f"Введите {i + 1} элемент массива: "))
         input_array.append(elem)
-        i += 1
     print("Исходный массив: ", input_array)
     return (input_array)
 
 def transform_array(array):
     output_array = []
-    j = 0
     elem = 0
     for j in range(len(array)):
         elem += int(array[j])
         output_array.append(elem)
-        j += 1
     print("Новый массив: ", output_array)
 
 def main():
